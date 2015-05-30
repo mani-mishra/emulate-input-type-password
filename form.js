70f66a3d6e7e8b31635931c2bf5d0d712be860e1
Mani Mishra <manimishra902@gmail.com>
2015-05-30 16:18:11 +0530

    Fixes typo in comment

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 // Solution 0 - Smart Ass Solution 
-// Covert type of the password field ;)
+// Convert type of the password input box from 'text' to 'password' ;)
 
 var solution0 = function () {
     $('input[name="pass"]').prop('type', 'password');
